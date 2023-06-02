604c00b589ad47a571e5cf61a4bd5cf970ffa4b6
pothugunta charan <charanpothugunta@gmail.com>
2023-06-02 21:54:45 +0530

    grid

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Header from "./components/Header";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Cards />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
